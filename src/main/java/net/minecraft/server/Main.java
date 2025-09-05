be8c62bbcb7d69defc776c045f8323500930785f
RayfieldMC <prahladhbackup+221@gmail.com>
2025-09-05 07:27:46 +0530

    Hotfix

--- src/main/java/net/minecraft/server/Main.java
+++ src/main/java/net/minecraft/server/Main.java
@@ -2,7 +2,6 @@ package net.minecraft.server;
 
 public class Main {
     public static void main(String[] args) {
-        // delegate straight to your Wrapidly launcher
         rarfield.Launcher.main(args);
     }
 }
